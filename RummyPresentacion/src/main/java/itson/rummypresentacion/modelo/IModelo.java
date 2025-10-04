e34026d532b870d13af77598edbd7c137f5ed3d0
JuanBaldenegro248272 <juan.rascon248272@potros.itson.edu.mx>
2025-10-04 11:47:31 -0700

    Update IModelo.java
    En este cambio se definio la interfaz de Modelo
    La idea fue dejar claras las acciones principales que se pueden realizar dentro del juego, de manera que el controlador se comunique con el modelo sin depender de su implementación interna.

--- RummyPresentacion/src/main/java/itson/rummypresentacion/modelo/IModelo.java
+++ RummyPresentacion/src/main/java/itson/rummypresentacion/modelo/IModelo.java
@@ -4,10 +4,19 @@
  */
 package itson.rummypresentacion.modelo;
 
+import entidades.Ficha;
+import java.util.List;
+
 /**
  *
  * @author Dana Chavez
  */
 public interface IModelo {
     
+    void crearGrupo(List<Ficha> fichas, String jugadorId) throws Exception;
+    void terminarTurno(String jugadorId) throws Exception;
+    void pasarTurno() throws Exception;
+    void separarGrupo(String grupoId, String jugadorId) throws Exception;
+    void sustituirFicha(String jugadorId, String grupoId, Ficha fichaNueva) throws Exception;
+    void cambiarFicha(String jugadorId, String grupoId) throws Exception;
 }

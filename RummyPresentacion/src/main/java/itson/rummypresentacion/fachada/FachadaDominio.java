34e6bc4047067508ecd9e6df62a2e141da6bfddf
Adrián Castán <151794030+adcastan@users.noreply.github.com>
2025-10-05 11:01:19 -0700

    Fachada Dominio con Validadores

--- RummyPresentacion/src/main/java/itson/rummypresentacion/fachada/FachadaDominio.java
+++ RummyPresentacion/src/main/java/itson/rummypresentacion/fachada/FachadaDominio.java
@@ -4,10 +4,49 @@
  */
 package itson.rummypresentacion.fachada;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  *
  * @author Dana Chavez
  */
 public class FachadaDominio {
     
+    public boolean validarSecuencia(List<Integer> numero, List<String> color) throws Exception {
+        if (numero.isEmpty() || color.isEmpty()) {
+            throw new Exception("La lista vacia.");
+        }
+
+        String colorBase = color.get(0);
+        for (String colores : color) {
+            if (!colores.equalsIgnoreCase(colorBase)) {
+                return false;
+            }
+        }
+
+        List<Integer> ordenar = new ArrayList<>(numero);
+        Collections.sort(ordenar);
+        for (int i = 1; i < ordenar.size(); i++) {
+            if (ordenar.get(i) != ordenar.get(i - 1) + 1) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+
+    public boolean validarNumeroIgual(List<Integer> numero, List<String> color) throws Exception {
+        if (numero.isEmpty() || color.isEmpty()) {
+            return false;
+        }
+        int numeroBase = numero.get(0);
+        for(int num:numero){
+            if(num!=numeroBase){
+                return false;
+            }
+        }
+        return true;
+    }
 }

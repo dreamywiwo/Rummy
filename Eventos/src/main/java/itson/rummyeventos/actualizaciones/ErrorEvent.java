3023e169d8e16cf2672fb2fda8a9bc76de749827
Dana Chavez <danacg11@hotmail.com>
2025-11-21 02:12:01 -0700

    directorio, events y dtos
    proyectos de directorio, eventos y dtos

---
+++ Eventos/src/main/java/itson/rummyeventos/actualizaciones/ErrorEvent.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package itson.rummyeventos.actualizaciones;
+
+import itson.rummyeventos.base.EventBase;
+
+/**
+ *
+ * @author Dana Chavez
+ */
+public class ErrorEvent extends EventBase {
+    
+    private String mensajeError;
+    
+    public static final String TOPIC = "actualizaciones.estado";
+    public static final String EVENT_TYPE = "mensaje.error";
+    
+    public ErrorEvent(String mensajeError) {
+        super(TOPIC, EVENT_TYPE);
+        this.mensajeError = mensajeError;
+    }
+
+    public String getMensajeError() {
+        return mensajeError;
+    }
+
+    public void setMensajeError(String mensajeError) {
+        this.mensajeError = mensajeError;
+    }
+    
+}

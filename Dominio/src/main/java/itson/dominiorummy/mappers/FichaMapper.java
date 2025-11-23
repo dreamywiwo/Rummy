a2c205576f3ffd62671d52fa9c32b9f4c6232567
voiddstles <victoria.vzls24@gmail.com>
2025-11-22 20:52:37 -0700

    avance de dominio crearTurno

---
+++ Dominio/src/main/java/itson/dominiorummy/mappers/FichaMapper.java
@@ -0,0 +1,35 @@
+ package itson.dominiorummy.mappers;
+
+import itson.dominiorummy.entidades.Ficha;
+import itson.dominiorummy.entidades.Tablero;
+import itson.rummydtos.FichaDTO;
+import itson.rummydtos.TableroDTO;
+import java.util.List;
+import java.util.stream.Collectors;
+ 
+/**
+ * @author victoria
+ */
+
+public class FichaMapper { 
+
+    public static List<Ficha> toEntidad(List<FichaDTO> dtos) {
+        return dtos.stream().map(dto -> FichaMapper.toEntidad(dto)) .collect(Collectors.toList());
+    }
+    
+    public static Ficha toEntidad(FichaDTO dto) {
+        return new Ficha(dto.getId(), dto.getNumero(), dto.getColor()); 
+    }
+    
+    public static TableroDTO toDTO(Tablero tablero) {
+        return new TableroDTO();
+    }
+    
+    public static FichaDTO toDTO(Ficha ficha) {
+        return new FichaDTO(ficha.getNumero(), ficha.getColor(), ficha.getId()); 
+    }
+    
+    public static List<FichaDTO> toDTO(List<Ficha> fichas) {
+        return fichas.stream().map(ficha -> FichaMapper.toDTO(ficha)).collect(Collectors.toList());
+    }
+}

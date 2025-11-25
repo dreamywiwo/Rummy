713e34dac6b762a94c70fc467114dd16729be5ba
JuanBaldenegro248272 <juan.rascon248272@potros.itson.edu.mx>
2025-11-24 23:03:40 -0700

    Broker update
    Se movio el broker para dejarlo como en el diagrama de secuencia, todo se concentraba en una sola clase y se separo todo

--- Broker/src/main/java/com/mycompany/broker/Broker.java
+++ Broker/src/main/java/com/mycompany/broker/Broker.java
@@ -4,8 +4,9 @@
  */
 package com.mycompany.broker;
 
-import interfaces.IDispatcher;
-import interfaces.IReceptorComponente;
+import com.mycompany.conexioninterfaces.IDispatcher;
+import com.mycompany.conexioninterfaces.IReceptorComponente;
+import interfaces.IBroker;
 import itson.directorio.implementacion.ConnectionEndpoint;
 import itson.directorio.implementacion.Directorio;
 import itson.rummyeventos.base.EventBase;
@@ -20,70 +21,54 @@ import java.util.Optional;
  *
  * @author jrasc
  */
-public class Broker implements IReceptorComponente {
+public class Broker implements IReceptorComponente, IBroker {
 
     private final Directorio directorio;
     private final IDispatcher dispatcher;
     private final ISerializer serializer;
+    private final SubscriptionRegistry registry;
 
     private final Map<String, List<String>> rutas = new HashMap<>();
 
-    public Broker(Directorio directorio, IDispatcher dispatcher, ISerializer serializer, IReceptorComponente receptor) {
+    public Broker(Directorio directorio, IDispatcher dispatcher, ISerializer serializer, SubscriptionRegistry registry) {
         this.directorio = directorio;
         this.dispatcher = dispatcher;
         this.serializer = serializer;
+        this.registry = registry;
     }
 
     @Override
     public void recibirMensaje(String json) {
-        procesarMensaje(json);
+        publicar(json);
     }
 
-    public void procesarMensaje(String json) {
+    @Override
+    public void publicar(String json) {
         EventBase base = serializer.deserialize(json, EventBase.class);
 
         if (base == null || base.getEventType() == null) {
             return;
         }
+        String topic = base.getEventType();
 
-        String eventType = base.getEventType();
-
-        List<String> listaClientes = rutas.getOrDefault(eventType, new ArrayList<>());
-
-        for (String subID : listaClientes) {
-            Optional<ConnectionEndpoint> endpointOpt = directorio.getEndpoint(subID);
-            if (endpointOpt.isPresent()) {
-                ConnectionEndpoint endpoint = endpointOpt.get();
-                String jsonEnviar = serializer.serialize(base);
-                dispatcher.enviar(json, endpoint.getPort(), endpoint.getIp());
-            }
+        List<String> clientesID = registry.getSuscriptores(topic);
+        List<ConnectionEndpoint> endpoints = directorio.getEndpoints(clientesID);
+        for (ConnectionEndpoint endpoint : endpoints) {
+            System.out.println("Se envia a: " + endpoint.getClientId());
+            dispatcher.enviar(json, endpoint.getPort(), endpoint.getIp());
         }
-
     }
 
-    public void subscribe(String eventType, String clienteID, String ip, int port) {
+    @Override
+    public void suscribir(String topic, String clienteID, String ip, int port) {
         directorio.registerClient(clienteID, ip, port);
-        rutas.computeIfAbsent(eventType, k -> new ArrayList<>()).add(clienteID);
-        System.out.println("Cliente " + clienteID + " suscrito a " + eventType);
+        registry.addSuscriptor(topic, clienteID);
+        System.out.println("Se suscribio correctamente el jugador: " + clienteID);
     }
 
-    public void unsuscribe(String eventType, String clienteID) {
-        if (rutas.containsKey(eventType)) {
-            rutas.get(eventType).remove(clienteID);
-            if (rutas.get(eventType).isEmpty()) {
-                rutas.remove(eventType);
-            }
-        }
-        boolean estaSuscrito = false;
-        for (List<String> lista : rutas.values()) {
-            if (lista.contains(clienteID)) {
-                estaSuscrito = true;
-                break;
-            }
-        }
-
-        if (!estaSuscrito) {
-            directorio.removeClient(clienteID);
-        }
+    @Override
+    public void unsubscribe(String topic, String clienteID) {
+        registry.removeSuscriptor(topic, clienteID);
     }
+
 }

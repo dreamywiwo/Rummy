b6d0047f857209c3eeb55e66a6a8bd9775b86aa5
JuanBaldenegro248272 <juan.rascon248272@potros.itson.edu.mx>
2025-11-27 21:21:53 -0700

    cambio de nombres
    Cambio de nombres

--- Launcher/src/main/java/itson/rummy/launcher/Exe.java
+++ Launcher/src/main/java/itson/rummy/launcher/Exe.java
@@ -1,7 +1,7 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-package com.mycompany.main;
+package itson.rummy.launcher;
 
 import ensambladores.EnsambladorCliente;
 import ensambladores.EnsambladorDominio;
@@ -16,7 +16,7 @@ import java.util.List;
  *
  * @author Dana Chavez
  */
-public class Main {
+public class Exe {
 
     public static void main(String[] args) {
         String brokerIp = "127.0.0.1";
